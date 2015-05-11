ec67ec2650cd88265af9fdf3b93c542f063cbec3
Marton Kiss <marton.kiss@gmail.com>
2015-05-11 14:03:47 +0200

    Askbot module refactor
    Refactor askbot module:
    - deployment source from git instead of pip
    - separate init / install / config code
    - break site config into celeryd / cron / http / log / ssl / static parts
    - support of Ubuntu Precise 12.04LTS / Trusty 14.04LTS
    
    Notice: don't approve this patch until puppet run is enabled on ask.o.o
    because it breaks the production site. (need to refactor the related
    system-config ask.pp too)
    
    Change-Id: I560c24c3b09e4a8d09b23afa619a4cf361601cbe

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,71 +1,144 @@
-# Class: askbot
+# == Class: askbot
+# This class sets up an askbot site
 #
-# This class installs Askbot and main dependencies, like
-# postgresql or mysql driver, and other django libraries.
-# (django-redis-cache, django-haystack, pysolr, stopforumspam)
+# == Parameters
+#   - $www_group: group name for web writeable directories like upfiles and log
+#   - $www_user: user name for web process
+#   - $askbot_debug: set to true to enable askbot debug mode
+#   - $dist_root: root directory of distribution releases
+#   - $site_root: root directory of site config and assets
+#   - $site_name: fqdn of askbot site
 #
-# Parameters:
-#   - $db_provider: database provider (mysql | pgsql)
-#   - $askbot_version: pip package version of askbot
-#   - $redis_enabled: set to true if askbot using redis for cache
+#   Source repository:
+#   - askbot_repo: git repository of askbot source files
+#   - askbot_revision: branch of askbot repo used for deployment
 #
-# Actions:
-#   - Install Askbot
-#   - Install Askbot dependencies
+#   Custom askbot theme settings:
+#   - $custom_theme_enabled: set to true to enable custom themes, default: false
+#   - $custom_theme_name: name of custom theme set to default
+#
+#   Redis configuration:
+#   - $redis_enabled: set to true to use redis as cache backend
+#   - $redis_prefix: redis key prefix (required for multi-site setups)
+#   - $redis_port: port of redis service
+#   - $redis_max_memory: memory allocation for redis
+#   - $redis_bind: bind address of redis service
+#   - $redis_password: password required for redis connection
+#
+#   SSL Settings:
+#   - $site_ssl_enabled: set to true for SSL based vhost
+#   - $site_ssl_cert_file_contents: x509 certificate in pem format
+#   - $site_ssl_key_file_contents: the key of site certificate in pem format
+#   - $site_ssl_chain_file_contents: the issuer certs of site cert (optional)
+#   - $site_ssl_cert_file: file name of site certificate
+#   - $site_ssl_key_file: file name of the site certificate's key file
+#   - $site_ssl_chain_file: file name of the issuer certificates
+#
+#   Email configuration:
+#   - $smtp_host: hostname of smtp service used for email sending
+#   - $smtp_port: port of smtp service
+#
+#   Database provider and connection details:
+#   - $db_provider: database provider (mysql or pgsql)
+#   - $db_name: database name
+#   - $db_user: user name required for db connection
+#   - $db_password: password required for db connection
+#   - $db_host: database host
+#
+#   Solr support:
+#   - solr_enabled: set true to use solr as a search indexing engine
+#
+# == Actions
 #
 class askbot (
-  $db_provider = 'mysql',
-  $askbot_version = '0.7.50',
-  $redis_enabled = false,
+  $dist_root                    = '/srv/dist',
+  $site_root                    = '/srv/askbot-site',
+  $askbot_revision              = 'master',
+  $askbot_repo                  = 'https://github.com/ASKBOT/askbot-devel.git',
+  $www_group                    = 'www-data',
+  $www_user                     = 'www-data',
+  $db_provider                  = 'mysql',
+  $db_name                      = undef,
+  $db_user                      = undef,
+  $db_password                  = undef,
+  $db_host                      = 'localhost',
+  $askbot_debug                 = false,
+  $redis_enabled                = false,
+  $redis_prefix                 = 'askbot',
+  $redis_port                   = undef,
+  $redis_max_memory             = undef,
+  $redis_bind                   = undef,
+  $redis_password               = undef,
+  $site_ssl_enabled             = false,
+  $site_ssl_cert_file_contents  = undef,
+  $site_ssl_key_file_contents   = undef,
+  $site_ssl_chain_file_contents = undef,
+  $site_ssl_cert_file           = '',
+  $site_ssl_key_file            = '',
+  $site_ssl_chain_file          = '',
+  $site_name                    = undef,
+  $custom_theme_enabled         = false,
+  $custom_theme_name            = undef,
+  $solr_enabled                 = false,
+  $smtp_port                    = '25',
+  $smtp_host                    = 'localhost'
 ) {
-  include apache::mod::wsgi
 
-  case $db_provider {
-    'mysql': {
-      $package_deps = [ 'python-pip', 'python-dev', 'python-mysqldb' ]
-    }
-    'pgsql': {
-      $package_deps = [ 'python-pip', 'python-dev', 'python-psycopg2' ]
-    }
-    default: {
-      fail("Unsupported database provider: ${db_provider}")
-    }
+  class { 'askbot::install':
+    db_provider     => $db_provider,
+    dist_root       => $dist_root,
+    askbot_repo     => $askbot_repo,
+    askbot_revision => $askbot_revision,
+    redis_enabled   => $redis_enabled,
+    solr_enabled    => $solr_enabled,
   }
 
-  package { $package_deps:
-    ensure => present,
-  }
-
-  if $redis_enabled {
-    package { 'django-redis-cache':
-      ensure   => present,
-      provider => 'pip',
-      before   => Package['askbot'],
+  if !defined(File[$dist_root]) {
+    file { $dist_root:
+      ensure => directory,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755',
     }
   }
 
-  package { [ 'django-haystack', 'pysolr' ]:
-    ensure   => present,
-    provider => 'pip',
-    before   => Package['askbot'],
+  vcsrepo { "${dist_root}/askbot":
+    ensure   => latest,
+    provider => git,
+    revision => $askbot_revision,
+    source   => $askbot_repo,
+    require  => [ File[$dist_root], Package['git'] ],
   }
 
-  package { 'stopforumspam':
-    ensure   => present,
-    provider => 'pip',
-    before   => Package['askbot'],
-  }
-
-  package { 'askbot':
-    ensure   => $askbot_version,
-    provider => 'pip',
-    require  => Package[$package_deps],
-  }
-
-  file { '/srv/askbot-sites':
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
+  class { 'askbot::config':
+    site_root                    => $site_root,
+    dist_root                    => $dist_root,
+    www_group                    => $www_group,
+    db_provider                  => $db_provider,
+    db_name                      => $db_name,
+    db_user                      => $db_user,
+    db_password                  => $db_password,
+    db_host                      => $db_host,
+    askbot_debug                 => $askbot_debug,
+    redis_enabled                => $redis_enabled,
+    redis_prefix                 => $redis_prefix,
+    redis_port                   => $redis_port,
+    redis_max_memory             => $redis_max_memory,
+    redis_bind                   => $redis_bind,
+    redis_password               => $redis_password,
+    site_ssl_enabled             => $site_ssl_enabled,
+    site_ssl_cert_file_contents  => $site_ssl_cert_file_contents,
+    site_ssl_key_file_contents   => $site_ssl_key_file_contents,
+    site_ssl_chain_file_contents => $site_ssl_chain_file_contents,
+    site_ssl_cert_file           => $site_ssl_cert_file,
+    site_ssl_key_file            => $site_ssl_key_file,
+    site_ssl_chain_file          => $site_ssl_chain_file,
+    site_name                    => $site_name,
+    custom_theme_enabled         => $custom_theme_enabled,
+    custom_theme_name            => $custom_theme_name,
+    solr_enabled                 => $solr_enabled,
+    smtp_port                    => $smtp_port,
+    smtp_host                    => $smtp_host,
+    require                      => [ Vcsrepo["${dist_root}/askbot"], Class['askbot::install'] ],
   }
 }
--- manifests/theme/compass.pp
+++ manifests/theme/compass.pp
@@ -1,4 +1,4 @@
-# Define: askbot::site
+# Define: askbot::theme::compass
 #
 # This class installs the Ruby based bundler command and compiles
 # the Sass files of a custom theme. The theme must contain a
@@ -8,26 +8,33 @@
 #   - Install Ruby / Compass
 #   - Compile Sass files into Css stylesheets
 #
-define askbot::compass(
+define askbot::theme::compass(
 ) {
-  # add ruby, bundler packages if not defined somewhere else
-  if ! defined(Package['rubygems']) {
-    package { 'rubygems':
-      ensure => present,
+  if $::operatingsystem == 'Ubuntu' and $::lsbdistrelease == '12.04' {
+    # add ruby, bundler packages if not defined somewhere else
+    if ! defined(Package['rubygems']) {
+      package { 'rubygems':
+        ensure => present,
+      }
     }
-  }
 
-  if ! defined(Package['bundler']) {
+    if ! defined(Package['bundler']) {
+      package { 'bundler':
+        ensure   => latest,
+        provider => gem,
+        require  => Package['rubygems'],
+      }
+    }
+  } else {
+    # add bundler as a debian package
     package { 'bundler':
       ensure   => latest,
-      provider => gem,
-      require  => Package['rubygems'],
     }
   }
 
   # install bundle requirements in Gemfiles, compile Sass
   exec { "theme-bundle-install-${name}":
-    cwd         => "/srv/askbot-sites/${name}/themes",
+    cwd         => '/srv/askbot-site/themes',
     path        => ['/bin', '/usr/bin', '/sbin', '/usr/sbin', '/usr/local/bin'],
     logoutput   => on_failure,
     command     => 'bundle install',
@@ -36,13 +43,13 @@ define askbot::compass(
   }
 
   exec { "theme-bundle-compile-${name}":
-    cwd         => "/srv/askbot-sites/${name}/themes",
+    cwd         => '/srv/askbot-site/themes',
     path        => ['/bin', '/usr/bin', '/sbin', '/usr/sbin', '/usr/local/bin'],
     logoutput   => on_failure,
     command     => 'bundle exec compass compile',
     require     => Exec["theme-bundle-install-${name}"],
     refreshonly => true,
-    notify      => Exec["askbot-static-generate-${name}"],
+    # notify      => Exec["askbot-static-generate-${name}"],
   }
 
 }
