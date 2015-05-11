e4f94173a25b8f0cfda4b36ba96d65cd4c3ec9ca
Marton Kiss <marton.kiss@gmail.com>
2015-05-11 14:03:47 +0200

    Askbot module refactor
    Refactor askbot module:
    - deployment source from git instead of pip
    - separate init / install / config code
    - break site config into celeryd / cron / http / log / ssl / static parts
    - support of Ubuntu Precise 12.04LTS / Trusty 14.04LTS
    
    Notice: don't approve this patch until puppet run is enabled on ask.o.o
    because it breaks the production site. (need to refactor the related
    system-config ask.pp too)
    
    Depends-On: Iaa8488a3d7ab8b121404ac1ac39bd1620a868727
    Change-Id: I560c24c3b09e4a8d09b23afa619a4cf361601cbe

---
+++ manifests/site/http.pp
@@ -0,0 +1,14 @@
+# == Class: askbot::site::http
+# This class describes the http server configuration
+class askbot::site::http (
+  $site_root     = undef,
+  $site_template = 'askbot/askbot.vhost.erb',
+  $site_name     = undef,
+) {
+  apache::vhost { $site_name:
+    port     => 80,
+    priority => 10,
+    docroot  => $site_root,
+    template => $site_template,
+  }
+}
