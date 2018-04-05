3badb37d47a7b95a1c025580bd4190a58a9af875
Ian Wienand <iwienand@redhat.com>
2018-04-05 14:36:52 +1000

    Basic rspec test
    Basic apply test to see if things can work on Xenial...
    
    Depends-On: https://review.openstack.org/559178
    Change-Id: I9f4764dd417c59661e21252a04234a7d7388a270

--- manifests/install.pp
+++ manifests/install.pp
@@ -19,18 +19,16 @@ class askbot::install (
     ensure => present,
   }
 
-  if !defined(Package['virtualenv']) {
-    package { 'virtualenv':
-      ensure => present,
-    }
+  class { 'python':
+    dev     => 'present',
   }
 
   python::virtualenv { '/usr/askbot-env':
     ensure  => present,
+    version => '2',
     owner   => 'root',
     group   => 'root',
     timeout => 0,
-    require => Package['virtualenv'],
   }
 
   case $db_provider {
@@ -98,9 +96,10 @@ class askbot::install (
     path        => [ '/bin', '/sbin' , '/usr/bin', '/usr/sbin', '/usr/local/bin' ],
     command     => "/usr/askbot-env/bin/pip install -q -r ${dist_root}/askbot/askbot_requirements.txt",
     cwd         => "${dist_root}/askbot",
-    logoutput   => on_failure,
+    logoutput   => true,
     subscribe   => Git['askbot'],
     refreshonly => true,
+    require     => Python::Virtualenv['/usr/askbot-env'],
   }
 
   python::pip { 'stopforumspam':
