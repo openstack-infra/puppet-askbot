ac91b365c4591da7639cd0d4566336d619a32974
Ian Wienand <iwienand@redhat.com>
2018-04-09 14:56:55 +1000

    Don't require virtualenv package
    Use puppet-python's checks to ensure presence of virtualenv and dev
    packages.  This way we don't create conflicting Package['virtualenv']
    entries.
    
    Depends-On: https://review.openstack.org/559871
    Change-Id: I3435a2527f4712d068c5a3c43728bed6011e1ebd

--- manifests/install.pp
+++ manifests/install.pp
@@ -19,10 +19,9 @@ class askbot::install (
     ensure => present,
   }
 
-  if !defined(Package['virtualenv']) {
-    package { 'virtualenv':
-      ensure => present,
-    }
+  class { '::python':
+    dev        => 'present',
+    virtualenv => 'present',
   }
 
   python::virtualenv { '/usr/askbot-env':
@@ -30,7 +29,6 @@ class askbot::install (
     owner   => 'root',
     group   => 'root',
     timeout => 0,
-    require => Package['virtualenv'],
   }
 
   case $db_provider {
