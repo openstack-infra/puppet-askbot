cb327fc1226ce0105984f6889e300b033dc62c75
Ian Wienand <iwienand@redhat.com>
2018-04-09 21:00:36 +1000

    fix captca version
    needs to be pinned
    
    Change-Id: I991c503f9f4a7e01be7ee58ba496f8c4f671c1a1

--- manifests/install.pp
+++ manifests/install.pp
@@ -65,7 +65,7 @@ class askbot::install (
 
   python::pip { 'captcha':
     pkgname    => 'captcha',
-    ensure     => '0.2.1'
+    ensure     => '0.2.1',
     virtualenv => '/usr/askbot-env',
     require    => Python::Virtualenv['/usr/askbot-env'],
   }
